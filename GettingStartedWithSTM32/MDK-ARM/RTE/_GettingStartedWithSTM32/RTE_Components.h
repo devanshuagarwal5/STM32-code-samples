fe48007a60e2807c96ba48a491e4a32b25ad5dc0
Devanshu <devanshuagarwal5@gmail.com>
2020-01-13 22:32:11 +0530

    Initial commit

---
+++ GettingStartedWithSTM32/MDK-ARM/RTE/_GettingStartedWithSTM32/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GettingStartedWithSTM32' 
+ * Target:  'GettingStartedWithSTM32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
